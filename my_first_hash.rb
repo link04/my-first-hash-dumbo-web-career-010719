af247379803bc812590ced08032dbd50b3f6f69d
Maximo Bautista <max.bautista97@gmail.com>
2018-12-26 15:29:16 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,13 +1,10 @@
 def my_hash
   my_hash = {"key" => "value", "key2" => "value"}
-
-  
 end
 
 
 def shipping_manifest
   the_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
-
 end
 
 def retrieval
