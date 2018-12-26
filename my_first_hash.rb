667b7bc04473a50234d37210b43bd048636e346e
Maximo Bautista <max.bautista97@gmail.com>
2018-12-26 15:30:52 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -26,7 +26,8 @@ def adding
                  
                  }
   # add 2 muskets to the shipping_manifest hash below
-
+       shipping_manifest["gun powder"] = 4
+       -"muskets" => 2
 
 
   # add 4 gun powder to the shipping_manifest hash below
